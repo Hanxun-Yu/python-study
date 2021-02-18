4fab18c3904ac423379a6d2bd888369c93db69cf
wsyuhx <wsyuhx@sunnyoptical.com>
2021-02-18 16:46:56 +0800

    add exception

--- python/exception.py
+++ python/exception.py
@@ -11,16 +11,45 @@
 
 
 def catch_exception():
+    """
+        try:
+            ...
+        except <Exception_1> as ex:
+            ...
+        except <Exception_2> as ex:
+            ...
+        finally:
+            ...
+    """
     try:
         fh = open("testfile", "r")
-    
     # except IOError:  # 简易写法 ，无法读取异常信息
-    except IOError as e: 
-        print("catch exception:", e)
+    except IOError as ex:
+        print("catch exception:", ex)
         print("Error: 没有找到文件或读取文件失败")
+    except Exception as ex:
+        print("catch exception:", ex)
+        print("未被上面准确捕获，这里使用万能异常捕获")
     finally:
         print("finally")
 
+
+def catch_exception_2():
+    """
+            try:
+                ...
+            except (<Exception_1>, <Exception_2>) as ex:
+                ...
+            finally:
+                ...
+        """
+    try:
+        fh = open("testfile", "r")
+    except (IOError, Exception) as ex:
+        print("catch exception:", ex)
+        print("Error: 没有找到文件或读取文件失败")
+    finally:
+        print("finally")
 
 def throw_exception():
     try:
@@ -33,4 +62,7 @@ def throw_exception():
 
 
 if __name__ == '__main__':
-    catch_exception()
+    # throw_exception()
+    # catch_exception()
+    catch_exception_2()
+
